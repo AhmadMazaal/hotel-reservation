e827b5a1dcfba864cf029bc1ab9ed442bc013146
Ahmad Mazaal <Ahmad.hmazaal@gmail.com>
2023-04-20 06:33:27 +0100

    add notes

--- src/HotelDriver.java
+++ src/HotelDriver.java
@@ -2,9 +2,15 @@ import menu.MainMenu;
 
 // ------- IMPROVEMENTS  -------
 //TODO: Seeders should only communicate with the API layer, so far it is not
-//TODO: Complete Seeders functionality
+//FIXME: Complete Seeders functionality
 //TODO: Document the project with the material learned
 // ------- IMPROVEMENTS  -------
+
+// ------- NOTES  -------
+// I submitted the complete project but with an incomplete requirements for the "Suggestions to Make Your Project Stand Out!" section to make it before the deadline if there's any feedback.
+// Kindly note that I have a repo for the code in my account, but it's private. Let me know if you need any access so I can share whole experience!
+// Would love to hear your feedback!
+// ------- NOTES  -------
 
 public class HotelDriver {
     public static void main(String []args){
